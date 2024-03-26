9f6bc0f4ead05775064b28a87fa6407dfc97a0c4
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-03-25 21:05:26 -0300

    spring security pt1

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,6 +1,7 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -8,17 +9,12 @@ import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String authority;
 
-    @ManyToMany
-    @JoinTable(name = "tb_user_role",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
     public Role(){
     }
     public Role(long id, String authority) {
@@ -34,25 +30,13 @@ public class Role {
         this.id = id;
     }
 
+   @Override
     public String getAuthority() {
         return authority;
     }
 
     public void setAuthority(String authority) {
         this.authority = authority;
-    }
-
-    public void addRole(Role role){
-        roles.add(role);
-    }
-
-    public Boolean hasRole(String roleName){
-        for (Role role : roles){
-            if (role.getAuthority().equals(roleName)){
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
